bddb3456cc1230198a4843002a6d1cdcd7ad1326
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-11-22 14:09:04 +0700

    Add document and refactor the code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
 use k256::{
+    elliptic_curve::{
+        group::GroupEncoding,
+        ops::Reduce,
+        sec1::{Coordinates, ToEncodedPoint},
+        Field,
+    },
     ProjectivePoint, Scalar, U256,
 };
+use sha2::{Digest, Sha256};
+
 fn generate_random_number() -> Scalar {
     let mut rng = rand::thread_rng();
     Scalar::random(&mut rng)
 }
+
 #[derive(Debug)]
 struct DLogProof {
     t: ProjectivePoint,
     s: Scalar,
 }
+
 impl DLogProof {
     fn new(t: ProjectivePoint, s: Scalar) -> Self {
         Self { t, s }
-}
+    }
+
+    /// Computes a hash of the given session id, point id, and points.
+    ///
+    /// This function takes a session id, a point id, and a vector of points, and computes a hash
+    /// of these inputs. The hash is then reduced to a scalar in the field of the elliptic curve.
+    ///
+    /// # Arguments
+    ///
+    /// * `sid` - The session id.
+    /// * `pid` - The id of the point.
+    /// * `points` - The points to be hashed.
+    ///
+    /// # Returns
+    ///
+    /// A `Scalar` representing the hash of the inputs.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use zk_proof::DLogProof;
+    /// # use k256::ProjectivePoint;
+    /// let sid = "sid";
+    /// let pid = 1;
+    /// let points = vec![ProjectivePoint::generator(); 3];
+    /// let hash = DLogProof::hash_points(sid, pid, &points);
+    /// println!("{}", hash);
+    /// ```
     fn hash_points(sid: &str, pid: u32, points: &[ProjectivePoint]) -> Scalar {
         let mut hasher = Sha256::new();
         hasher.update(sid.as_bytes());
@@ -26,6 +63,38 @@ impl DLogProof {
         let e = <Scalar as Reduce<U256>>::reduce_bytes(result.into());
         e
     }
+
+    /// Generates a proof that the prover knows the discrete logarithm of `y`.
+    ///
+    /// The prover generates a random number `r`, computes `t = r*G` and `c = H(sid, pid, G, y, t)`,
+    /// and then computes `s = r + c*x` and returns the proof `(t, s)`.
+    ///
+    /// # Arguments
+    ///
+    /// * `sid` - The session id.
+    /// * `pid` - The id of the prover.
+    /// * `x` - The secret number.
+    /// * `y` - The point that we want to prove that we know the discrete logarithm of.
+    /// * `base_point` - The base point of the group.
+    ///
+    /// # Returns
+    ///
+    /// A `DLogProof` struct containing the `t` and `s` values.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use zk_proof::DLogProof;
+    /// # use zk_proof::generate_random_number;
+    /// # use k256::ProjectivePoint;
+    /// let sid = "sid";
+    /// let pid = 1;
+    /// let x = generate_random_number();
+    /// let y = &x * &ProjectivePoint::generator();
+    /// let dlog_proof = DLogProof::prove(sid, pid, x, y);
+    /// println!("{}, {}", dlog_proof.t.x(), dlog_proof.t.y());
+    /// println!("{}", dlog_proof.s);
+    /// ```
     fn prove(sid: &str, pid: u32, x: Scalar, y: ProjectivePoint) -> Self {
         let r = generate_random_number();
         let t = ProjectivePoint::GENERATOR * &r;
@@ -33,12 +102,58 @@ impl DLogProof {
         let s = r + c * x;
         Self::new(t, s)
     }
+
+    /// Verifies that the point `t` equals `s` times the base point plus the hash of the inputs times `y`.
+    ///
+    /// # Arguments
+    ///
+    /// * `sid` - The session id.
+    /// * `pid` - The id of the prover.
+    /// * `y` - The public key.
+    /// * `base_point` - The base point of the group.
+    ///
+    /// # Returns
+    ///
+    /// A boolean indicating whether the point `t` is the correct sum.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use zk_proof::DLogProof;
+    /// # use zk_proof::generate_random_number;
+    /// # use k256::ProjectivePoint;
+    /// let sid = "sid";
+    /// let pid = 1;
+    /// let x = generate_random_number();
+    /// let y = &x * &ProjectivePoint::generator();
+    /// let dlog_proof = DLogProof::prove(sid, pid, x, y);
+    /// assert!(dlog_proof.verify(sid, pid, y));
+    /// ```
     fn verify(&self, sid: &str, pid: u32, y: ProjectivePoint) -> bool {
         let c = Self::hash_points(sid, pid, &[ProjectivePoint::GENERATOR, y, self.t]);
         let lhs = ProjectivePoint::GENERATOR * &self.s;
         let rhs = &self.t + &(&y * &c);
         lhs == rhs
     }
+
+    /// Converts the `DLogProof` instance into a JSON object.
+    ///
+    /// This method serializes the `DLogProof` instance into a `serde_json::Value` object.
+    /// The `t` field of the `DLogProof` is converted to affine coordinates and then to bytes,
+    /// and the `s` field is directly converted to bytes. Both are then stored in a JSON object.
+    ///
+    /// # Returns
+    ///
+    /// A `serde_json::Value` representing the `DLogProof` instance.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use zk_proof::DLogProof;
+    /// let dlog_proof = DLogProof::new(/* parameters */);
+    /// let json = dlog_proof.to_dict();
+    /// println!("{}", json);
+    /// ```
     fn to_dict(&self) -> serde_json::Value {
         serde_json::json!({
             "t": self.t.to_affine().to_bytes().to_vec(),
@@ -59,10 +174,12 @@ impl DLogProof {
         let s = <Scalar as Reduce<U256>>::reduce_bytes(s_bytes.into());
         Self::new(t, s)
     }
-  }
+}
+
 fn main() {
     let sid = "sid";
-    let pid: u32 = 1;
+    let pid = 1;
+
     let x = generate_random_number();
     println!("x: {:?}", x);
     let y = ProjectivePoint::GENERATOR * &x;
@@ -95,4 +212,4 @@ fn main() {
     } else {
         println!("DLOG proof is not correct");
     }
-    }
+}
